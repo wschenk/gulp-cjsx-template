ac2dc66c2a0bb2ddd83375863207c8b175ac8381
Will Schenk <wschenk@gmail.com>
2015-07-08 18:41:16 -0400

    Forgot to change a config file

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -8,13 +8,13 @@ gulp.task( 'js:watch', ['javascript'], browserSync.reload );
 
 gulp.task( 'watch', ['build'], function() {
   gulp.watch(config.html.source, ['html']);
-  gulp.watch(config.js.srcDir + "**", ['js_watch']);
+  gulp.watch(config.js.srcDir + "**", ['js:watch']);
   gulp.watch(config.sass.srcDir, function() {
     return sass_task().pipe(browserSync.stream());
   });
   browserSync.init({
     server: {
-            baseDir: config.destDir
+            baseDir: config.devDir
         }
   });
 });
